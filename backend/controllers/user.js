33f36d0b9658069a06d0428e687d5d5ab82210e4
ItoKobi <ito.kobilol@gmail.com>
2021-08-12 21:25:37 -0400

    Added JWT Login Route & Authentication

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,3 +1,34 @@
+const express = require("express");
+
+const { User } = require("../models/user.models.js");
+const { generateAccessToken } = require("../utils");
+
 module.exports.mainRoute = (req, res) => {
-  res.send("Working");
+  User.find({}, (err, rest) => {
+    res.json(rest)
+  })
+}
+
+module.exports.loginRoute = (req, res) => {
+  try{
+    const {email, password} = req.body;
+    let returnObject = {};
+    User.findOne({email: email}, (err, result) => {
+      if(err) return res.status(400).json(err);
+      else {
+        if(result !== null && result.password === password){
+          returnObject["token"] = generateAccessToken({
+            id: result["_id"],
+            secret_message: "hi kobi"
+          });
+          returnObject["message"] = "Success.";
+          return res.status(200).json(returnObject);
+        }else{
+          return res.status(202).json({"message": "not authenticated."})
+        }
+      }
+    })
+  }catch{
+    return res.status(400).json({"message": "Error."})
+  }
 }
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,9 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 const {
-  mainRoute
+  auth
+} = require("../middleware/auth.js");
+
+const {
+  mainRoute,
+  loginRoute
 } = require("../controllers/user.js");
 
 router.get("/", mainRoute)
+router.post("/login", loginRoute);
 
 module.exports.UserRoutes = router;
